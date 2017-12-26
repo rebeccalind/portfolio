9c87c900ba590b1fdf87bc8286106754fb8e447c
Rebecca Lind <32701610+rebeccalind@users.noreply.github.com>
2017-12-26 16:04:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -178,3 +178,7 @@ function keepHigh6() {
     document.getElementById("myMenu4").style.color = "#b4b4b4";
     document.getElementById("myMenu5").style.color = "#b4b4b4";
 }
+
+//SCROLL
+
+document.getElementById("myNav2").addEventListener("scroll", openNav3);
